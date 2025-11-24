3171db31857e37bb3b7087b9aeead86ca61fa18f
Trishan Wagle <mailtotrishan@gmail.com>
2025-11-24 23:50:59 +0545

    fix: add strict in dtos to avoid unrecognized keys

--- src/dtos/user.dto.ts
+++ src/dtos/user.dto.ts
@@ -1,20 +1,25 @@
 import { z } from "zod";
 import { userSchema } from "../types/user.type";
 
-export const CreateUserDTO = userSchema.pick({
-  id: true,
-  username: true,
-  email: true,
-  name: true,
-  age: true,
-});
+export const CreateUserDTO = userSchema
+  .pick({
+    id: true,
+    username: true,
+    email: true,
+    name: true,
+    age: true,
+  })
+  .strict();
 
-export const UpdateUserDTO = userSchema.partial().pick({
-  username: true,
-  email: true,
-  name: true,
-  age: true,
-});
+export const UpdateUserDTO = userSchema
+  .partial()
+  .pick({
+    username: true,
+    email: true,
+    name: true,
+    age: true,
+  })
+  .strict();
 
 export type TCreateUserDTO = z.infer<typeof CreateUserDTO>;
 export type TUpdateUserDTO = z.infer<typeof UpdateUserDTO>;
